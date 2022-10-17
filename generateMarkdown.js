67701f25f5aa8c7a78e7f1d25993423cde116608
Mtomchik <matthew.tomchik@yahoo.com>
2022-10-17 10:23:44 -0600

    generator code updated

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = license;
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'Apache') {
+    yourLicense = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  }  else if (licenseType === 'GNU') {
+    yourLicense = `![License: GNU ](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  }else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+};
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## License 
+${renderLicenseBadge(license)}
+`};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseType = license;
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://opensource.org/licenses/MIT)`
+  } else if (licenseType === 'Apache') {
+    yourLicense = `![License](https://opensource.org/licenses/Apache-2.0)`
+  }  else if (licenseType === 'GNU') {
+    yourLicense = `![License: GNU](https://www.gnu.org/licenses/gpl-3.0)`
+  }else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+};
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## License 
+${renderLicenseLink(license)}
+`}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
--- index.js
+++ index.js
@@ -1,34 +1,109 @@
 // TODO: Include packages needed for this application
-const inquirer = require ('inquirer')
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    },
-    {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
-    },
-    {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
-    },
-  ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
-const questions = [];
+inquirer.prompt ([
+   {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+   },
+   {
+    type: 'input',
+    message: 'Describe your application.',
+    name: 'description',
+   },
+   {
+    type: 'input',
+    message: 'How do you install your application?',
+    name: 'installation',
+   },
+   {
+    type: 'input',
+    message: 'How is this application used?',
+    name: 'use',
+   },
+   {
+    type: 'input',
+    message: 'How can someone contribute to this application?',
+    name: 'contributing',
+   },
+   {
+    type: 'input',
+    message: 'What are the testing instructions?',
+    name: 'tests',
+   },
+   {
+    type: 'list',
+    message: 'What licence would you like to use?',
+    name: 'licence',
+    choices:['MIT', 'Apache', 'GNU', 'N/A'],
+   },
+   {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'git',
+   },
+   {
+    type: 'input',
+    message: 'Where can questions about your application be sent?',
+    name: 'email',
+   },
+
+  ]
+).then (({
+  title,
+  description,
+  installation,
+  use,
+  contributing,
+  tests,
+  licence,
+  git,
+  email,
+
+})=>{
+const generate = `# ${title}
+
+*[Description](#description)
+*[Installation](#installation)
+*[Use](#use)
+*[Contributing](#contributing)
+*[Tests](#tests)
+*[Licence](#licence)
+*[Git](#git)
+*[Email](#email)
+# Description
+${description}
+## Installation
+${installation}
+## Use
+${use}
+## Contributing
+${contributing}
+## Tests
+${tests}
+## Licence
+${licence}
+## Git
+${git}
+## Email
+${email}`;
+
+writeToFile(title, generate);
+}
+)
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(`./${fileName.toLowerCase().split(' ').join(' ')}.md`, data,(err) => {
+    if(err){
+      console.log(err);
+    }
+    console.log('Your README has been generated');
+  })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
